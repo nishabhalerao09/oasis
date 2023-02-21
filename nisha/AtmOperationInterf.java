a6e1abfa3647af39dfa67a9d3d8e5bf5a8fde2b3
nishabhalerao <bhaleraonisha008@gmail.com>
2023-02-21 23:58:14 +0530

    First Commit

---
+++ nisha/AtmOperationInterf.java
@@ -0,0 +1,9 @@
+package nisha;
+
+public interface AtmOperationInterf {
+
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount);
+    public  void viewMiniStatement();
+}
